5be625b5f3268df4f488046769c4e1d3c4aceaca
jameswood <james_s_wood@outlook.com>
2021-03-23 20:39:16 -0400

    Initial commit

---
+++ src/com/creative/solutions/Main.java
@@ -0,0 +1,9 @@
+package com.creative.solutions;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Udacity");
+    }
+}
